e7a5c6d8f1335dbb90450e8bd4e60dd007d64a80
othmane <othmanechahid1@gmail.com>
2022-03-17 10:36:11 +0100

    initial commit

---
+++ migration.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+print("Creating DB")
+conn = sqlite3.connect("fb.db")
+print("DB created")
+conn.execute(
+    """CREATE TABLE if not exists facebook (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username char(100) NOT NULL UNIQUE, email char(100) NOT NULL,
+    password char(100) NOT NULL, cookie char(128) UNIQUE)"""
+ )
+conn.execute("INSERT INTO facebook (username, email, password) VALUES ('Vivien', 'vivien@facebook.com', 'TEST123')")
+conn.execute("INSERT INTO facebook (username, email, password) VALUES ('Elsa', 'elsa@facebook.com', 'TEST124')")
+conn.execute("INSERT INTO facebook (username, email, password) VALUES ('Anna', 'anna@facebook.com', 'TEST125')")
+conn.commit()
+print("Table created")
